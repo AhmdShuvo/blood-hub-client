5930581ddf485a06e0c6fc0c020c7b7b0ae5bafa
Arif reza <arif.reza3126@gmail.com>
2022-03-24 01:45:45 +0600

    first commit

---
+++ src/Pages/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXsD9u4yzZkmrI66SzNk-skf1z6r8Ortw",
+    authDomain: "bloodhub-7933f.firebaseapp.com",
+    projectId: "bloodhub-7933f",
+    storageBucket: "bloodhub-7933f.appspot.com",
+    messagingSenderId: "625926856257",
+    appId: "1:625926856257:web:9f3ab0058c263f21fa30c0"
+};
+
+export default firebaseConfig;
