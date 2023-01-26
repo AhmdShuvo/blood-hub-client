232997ddf0861240b5de313017c47de59223ab0e
AhmdShuvo <tanzirshovo2@gmail.com>
2023-01-26 15:07:27 +0600

    Pagination

--- src/Pages/Service/Pagination.js
+++ src/Pages/Service/Pagination.js
@@ -1,56 +1,33 @@
 import React from 'react';
 
-const Pagination = () => {
-    return (
-        <div>
-            <div className="flex flex-col items-center">
-  {/* Help text */}
-  <span className="text-sm text-gray-700 dark:text-gray-400">
-    Showing{" "}
-    <span className="font-semibold text-gray-900 dark:text-white">1</span> to{" "}
-    <span className="font-semibold text-gray-900 dark:text-white">10</span> of{" "}
-    <span className="font-semibold text-gray-900 dark:text-white">100</span>{" "}
-    Entries
-  </span>
-  <div className="inline-flex mt-2 xs:mt-0">
-    {/* Buttons */}
-    <button className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-      <svg
-        aria-hidden="true"
-        className="mr-2 w-5 h-5"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-      Prev
-    </button>
-    <button className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-      Next
-      <svg
-        aria-hidden="true"
-        className="ml-2 w-5 h-5"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </button>
-  </div>
-</div>
+const Pagination = ({totalpost,postPerPage,setCurrentPage,currentPage}) => {
+  let pages=[];
 
-        </div>
-    );
+  for (let i =1;i<=Math.ceil(totalpost/postPerPage);i++){
+    pages.push(i)
+  }
+  return (
+    <nav aria-label="Page navigation example" className='flex content-center justify-center mt-5'>
+  <ul class="inline-flex -space-x-px">
+  <li>
+      <button onClick={()=>{ currentPage!=1&& setCurrentPage(currentPage-1)
+      console.log(currentPage);
+      }} class="px-3 mx-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</button>
+    </li>
+    {
+      pages.map((page,index)=><li>
+        <button key={index} onClick={()=>setCurrentPage(page)} class={`px-3 mx-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${currentPage===page &&"bg-red-600 text-zinc-200"}`}>{page}</button>
+      </li>)
+    }
+    <li>
+      <button onClick={()=>{ currentPage!=pages.length && setCurrentPage(currentPage+1)
+      console.log(currentPage);
+      }} class="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</button>
+    </li>
+    
+  </ul>
+</nav>
+  );
 };
 
 export default Pagination;
--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Pagination from './Pagination';
 import SearchBar from './SearchBar';
 
 const Service = () => {
-    const services=[1,2,3,4,5,6,7,8,9,0,11,22,33,44,55,66,77,88,99]
-
+  const [currentpost,setCurrentPost]=useState(1)
+  const [postPerPage,setpostPerage]=useState(5)
+    const services=[1,2,3,4,5,6,7,8,9,0,11,22,33,44,55,66,77,88,99,100,101,102]
+    
+    const lastIndex=currentpost * postPerPage;
+    const startIndex=lastIndex-postPerPage;
     return (
         <>
         <section className='search-bar mb-24'>
             <SearchBar/>
             <div className="grid md:grid-cols-3 ms-5 gap-1 ml-8">
             {
-                services.map(service=><a
-                    href=" #"
+                services.slice(startIndex,lastIndex).map(service=><a
+                    href=" #" 
                     className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
                   >
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      Noteworthy technology acquisitions 2021
+                      Noteworthy technology acquisitions 2021 num:{service}
                     </h5>
                     <p className="font-normal text-gray-700 dark:text-gray-400">
                       Here are the biggest enterprise technology acquisitions of 2021 so far, in
@@ -26,7 +30,13 @@ const Service = () => {
                   )
             }
             </div>
-            <Pagination/>
+            {/* Pagination / */}
+            
+            <Pagination totalpost={services.length}
+            postPerPage={postPerPage}
+            currentPage={currentpost}
+            setCurrentPage={setCurrentPost} />
+
         </section>
         </>
             );
